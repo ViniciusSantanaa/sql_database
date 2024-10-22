552e24d309dc9d8fd6ee6ee72298ba0f01c8f8bc
labsfiap <labsfiap@PA501MICRO24.fiap.com.br>
2024-10-21 22:22:20 -0300

    SEGUNDO COMMIT, CRIANDO O DML E INSERINDO VALORES

---
+++ DML.sql
@@ -0,0 +1,17 @@
+INSERT INTO tb_endereco (
+    rua,
+    numero,
+    cep,
+    bairro,
+    cidade,
+    pais,
+    complemento
+) VALUES (
+    'AV HILARIO PEREIRA',
+    492,
+    '06010170',
+    'CENTRO',
+    'OSASCO',
+    'BRASIL',
+    'CASA DOS GEMEOS'
+);
